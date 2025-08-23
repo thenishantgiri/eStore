ae9efe087f3131a051f68b7096d6107759c062af
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2025-08-23 23:17:41 +0530

    Global Style: implemented global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,29 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Jost} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const jost = Jost({
+    variable: "--font-jost",
+    subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
-  title: "eStore - Next.js E-commerce Starter",
-  description: "developed by Loonar Labs",
+    title: "eStore - Next.js E-commerce Starter",
+    description: "developed by Loonar Labs",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    return (
+        <html lang="en">
+        <body
+            className={`${jost.variable} ${jost.variable} antialiased`}
+        >
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
