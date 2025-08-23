c4df03087aab9f254863bdcbc87274415bf8c9be
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2025-08-24 00:22:45 +0530

    UI: Implemented initial landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,31 @@
-import type {Metadata} from "next";
-import {Jost} from "next/font/google";
+import type { Metadata } from "next";
+import { Jost } from "next/font/google";
 import "./globals.css";
+import { Navbar, Footer } from "@/components";
 
 const jost = Jost({
-    variable: "--font-jost",
-    subsets: ["latin"],
-})
+  variable: "--font-jost",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-    title: "eStore - Next.js E-commerce Starter",
-    description: "developed by Loonar Labs",
+  title: "Nike",
+  description: "An e-commerce platform for Nike shoes",
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-        <body
-            className={`${jost.variable} ${jost.variable} antialiased`}
-        >
+  return (
+    <html lang="en">
+      <body className={`${jost.className} antialiased`}>
+        <Navbar />
+
         {children}
-        </body>
-        </html>
-    );
+        <Footer />
+      </body>
+    </html>
+  );
 }
